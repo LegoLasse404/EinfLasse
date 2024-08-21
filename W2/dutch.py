8ef0b5a37e887c87ca3270923e675fdaae6ca980
Lasse <lasselandwehr@icloud.com>
2024-08-21 16:32:24 +0200

    dutch

--- W2/dutch.py
+++ W2/dutch.py
@@ -3,17 +3,18 @@ liste = [0,1,2,0,1,2,0]
 offset = 0
 
 print(liste)
-print(liste)
+
 for i in range(len(liste)):
     if liste[i+offset] == 2:
         liste.append(liste[i+offset])
-        print(liste)
         liste.pop(i+offset)
         offset -= 1
-    elif liste[i+offset] == 0:
+        print("2:", liste)
+    if liste[i+offset] == 0:
         liste.insert( 0, liste[i+offset])
-        print(liste)
-        liste.pop(i+offset)
         offset += 1
-
+        liste.pop(i+offset)
+        print("0:", liste)
+    else:
+        print("1:", liste)
 print(liste)
